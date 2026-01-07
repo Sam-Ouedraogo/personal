9a27974cb18f6e36e470c717bd5880dbd458db2c
Sam <owsam@outlook.fr>
2026-01-07 01:01:09 -0800

    fixed resume routiing

--- public/script.js
+++ public/script.js
@@ -1,37 +1,69 @@
 "use strict";
 
 document.querySelector('.linkedIn-icon')?.addEventListener('click', () => {
-    window.location.href = 'https://linkedin.com';
+    // window.location.href = 'https://linkedin.com';
+    window.open('https://linkedin.com', '_blank');
   });
 
 document.querySelector('.github-icon')?.addEventListener('click', () => {
-    window.location.href = 'https://github.com';
+    window.open('https://github.com', '_blank');
   });
 
 document.querySelector('.x-icon')?.addEventListener('click', () => {
-    window.location.href = 'https://x.com';
+    window.open('https://x.com', '_blank');
   });
 
 document.querySelector('.about_me-icon')?.addEventListener('click', () => {
-    window.location.href = './#about';
+    window.open('./#about', '_self');
   });
 
 document.querySelector('.portfolio-icon')?.addEventListener('click', () => {
-    window.location.href = './#projects';
+    window.open('./#projects', '_self');
   });
 
-document.querySelector('.resume-icon')?.addEventListener('click', () => {
-    window.location.href = './public/assets/Resume.pdf';
+document.querySelectorAll('.resume, .resume-icon').forEach(el => {
+  el.addEventListener('click', () => {
+    window.open('./assets/Resume.pdf', '_blank');
   });
+});
+
+
+
+
+// document.querySelector('.resume-icon')
+//   ?.closest('li')
+//   ?.addEventListener('click', () => {
+//     window.open('./public/assets/Resume.pdf', '_blank');
+//   });
+
 
 document.querySelector('.contact-icon')?.addEventListener('click', () => {
     window.location.href = './#contact';
   });
 
-const themeToggleBtn = document.querySelector('.toggle-theme');
-themeToggleBtn?.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
+
+
+const toggleBtn = document.getElementById("theme-toggle");
+const root = document.documentElement;
+
+// Load saved theme or system preference
+const savedTheme = localStorage.getItem("theme");
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const theme = savedTheme || (prefersDark ? "dark" : "light");
+root.setAttribute("data-theme", theme);
+toggleBtn.textContent = theme === "dark" ? "☀️" : "🌙";
+
+// Toggle on click
+toggleBtn.addEventListener("click", () => {
+  const current = root.getAttribute("data-theme");
+  const next = current === "dark" ? "light" : "dark";
+
+  root.setAttribute("data-theme", next);
+  localStorage.setItem("theme", next);
+  toggleBtn.textContent = next === "dark" ? "☀️" : "🌙";
 });
+
 
 
 function toggleMenu () {
